1803320f45984a2ae63c9c222b70771237b86b03
sulthan441 <mohammedsulthan441@gmail.com>
2023-02-24 16:37:45 +0530

    done

--- server/routes/route.js
+++ server/routes/route.js
@@ -5,12 +5,17 @@ import { addTodo, getAllTodos, toggleTodoDone, updateTodo, deleteTodo } from '..
 
 const route = express.Router();
 
-
+// route.get("/hello" ,(req,res)=>{
+//     res.send("hello world!");
+//     console.log("hello world!");
+  
+// })
 route.post('/todos', addTodo)
 route.get('/todos', getAllTodos);
 route.get('/todos/:id', toggleTodoDone);
 route.put('/todos/:id', updateTodo);
 route.delete('/todos/:id', deleteTodo);
+
 
 
 export default route;
